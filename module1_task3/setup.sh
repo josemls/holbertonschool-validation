e9030e9bd276112ed2ecc420f72cdc0d0ade1df9
josemls <jose_9513@hotmail.com>
2023-08-11 22:31:11 -0400

    setup

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Always attempt to update and install hugo and make, even if there's an error
-apt-get update || true
-apt-get install -y hugo || true
-apt-get install -y make || true
-
-# Check for config file or directory before executing make build
-if [[ ! -f "hugo.toml" && ! -d "public" ]]; then
-    echo "Error: Unable to locate config file or config directory"
-    exit 1
-fi
-
+apt-get update && apt-get install -y hugo make
 make build
